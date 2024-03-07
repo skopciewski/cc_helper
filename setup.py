942d4a0154e6012475c52aa8c351ef46534733d3
simonk <s.kopciewski@dlamnie.net>
2024-03-07 16:53:15 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='cc-helper',
+    version='0.1',
+    packages=find_packages(include=['cchelpers', 'cchelpers.*']),
+    install_requires=[
+        'markdown-it-py~=3.0.0',
+        'beautifulsoup4~=4.12.3',
+    ],
+    entry_points={
+        'console_scripts': [
+            'cc_compresor=cchelpers.compresor:init',
+            'cc_validator=cchelpers.validator:init',
+        ],
+    },
+)
