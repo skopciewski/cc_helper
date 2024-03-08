ddd993901f775b94a810da5589116f0181192de8
simonk <s.kopciewski@dlamnie.net>
2024-03-08 20:16:35 +0100

    Tests with write

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cc-helper',
-    version='0.4.0',
+    version='0.5.0',
     packages=find_packages(include=['cchelpers', 'cchelpers.*']),
     install_requires=[
         'markdown-it-py~=3.0.0',
